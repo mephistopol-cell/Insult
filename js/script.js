1e8bc0064e0a89835f2948e3671e17ac2800dbfa
unknown <mephistopol@gmail.com>
2026-02-24 16:35:46 +0400

    lll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+let obj = {
+
+};
+
+obj["nick"] = prompt();
+obj["pasword"] = prompt();
+obj["username"] = prompt();
+
+
+console.log(obj)
+alert("Ваш ник " + obj.nick );
+alert("Ваш пароль " + obj.pasword);
+alert("Ваше имя " + obj.username);
