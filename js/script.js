ec4df8077f158ce94efc4e2ee2886d7723f83eba
unknown <mephistopol@gmail.com>
2026-03-06 17:44:35 +0400

    lll

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
 "use strict";
 
-function roots(a, b, c) {
+// function roots(a, b, c) {
 
-    let d = b*b - 4 * a * c;
-    let x = (-b + d**0.5)/(2 * a);
-    return x;
-}
-document.body.querySelector('button').addEventListener("click", () => {
-    let a = Number(document.querySelector(".one").value);
+//     let d = b*b - 4 * a * c;
+//     let x = (-b + d**0.5)/(2 * a);
+//     return x;
+// }
+document.body.querySelector('.plu').addEventListener("click", () => {
+let a = Number(document.querySelector(".one").value);
 let b = Number(document.querySelector(".two").value);
-let c = Number(document.querySelector(".three").value);
-document.querySelector('.res').style.font = "32pt"
-document.querySelector('.res').textContent = roots(a, b, c)
+let elem = document.createElement("div")
+elem.textContent = a + b;
+document.body.append(elem);
+
+})
+document.body.querySelector('.min').addEventListener("click", () => {
+let a = Number(document.querySelector(".one").value);
+let b = Number(document.querySelector(".two").value);
+let elem = document.createElement("div")
+elem.textContent = a - b;
+document.body.append(elem);
+
+})
+document.body.querySelector('.ras').addEventListener("click", () => {
+let a = Number(document.querySelector(".one").value);
+let b = Number(document.querySelector(".two").value);
+let elem = document.createElement("div")
+elem.textContent = a / b;
+document.body.append(elem);
+})
+document.body.querySelector('.umn').addEventListener("click", () => {
+let a = Number(document.querySelector(".one").value);
+let b = Number(document.querySelector(".two").value);
+let elem = document.createElement("div")
+elem.textContent = a / b;
+document.body.append(elem);
 
 })
